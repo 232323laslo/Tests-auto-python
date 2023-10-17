7e7ff263e5f427f69bf8f9cc9d05e1167350f441
laslo23 <vladyslav_hryshchenko@proton.me>
2023-10-17 18:19:49 +0300

    Add confest and fixture files

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -1,34 +1,29 @@
 import pytest
 import requests
 import re
+from bs4 import BeautifulSoup
+
 
 
 @pytest.mark.http
-def test_first_request():
+def test_website_response():
     r = requests.get("https://football.ua/")
     assert r.status_code == 200
 
 
-
 @pytest.mark.http
-def test_second_request():
+def test_if_user_exist():
     r = requests.get("https://football.ua/profile/e6876641-422e-4aea-a282-66c47a22f8cf.html")
     body = r.content.decode()
 
     match = re.search(r'alt=\"yashanet\"', body)
     assert match is not None
 
+
 @pytest.mark.http
-def test_third_request():
+def test_if_user_not_exist():
     r = requests.get("https://football.ua/profile/e6876641-422e-4aea-a282-66c47a22f8cf.html")
     body = r.content.decode()
 
     match = re.search(r'alt=\"yashanet23\"', body)
     assert match is None
-
-
-
-    #match = re.search(r"<title>(.*)</title>", body)
-    #print(match)
-    #if match:
-        #print(match.group(1))
